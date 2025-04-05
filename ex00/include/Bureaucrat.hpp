86de1550dcbb5510e81532e3a95597a514aa7bd8
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-04-05 22:49:58 +0200

    create cpp file

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 19:05:12 by cmakario          #+#    #+#             */
-/*   Updated: 2025/04/05 21:48:47 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/04/05 22:42:14 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define BUREAUCRAT_HPP
 
 # include <iostream>
+# include <stdexcept>
 
 
 class Bureaucrat
@@ -24,18 +25,19 @@ private:
 public:
 		//-----------OCF-----------------//
 		Bureaucrat();									// * Constructor (default)
+		Bureaucrat(const std::string& name, int grade);	// * Constructor with parameters
 		Bureaucrat(const Bureaucrat &src);				// * Copy Contructor
 		Bureaucrat &operator= (const Bureaucrat &src);	// * Copy Assignment operator
 		~Bureaucrat();									// * Destructor
 		//===============================//
-		//-----------Methods------------//
-		//----Getter-------------------//
+														//*-----------Methods------------//
+														//*----Getter-------------------//
 		std::string getName() const;
 		int getGrade() const;
-		//----------Inc/decrement-----//
+														//*----------Inc/decrement-----//
 		void incrementGrade();
 		void decrementGrade();
-		//-------Exceptions----------//
+														//* -------Exceptions----------//
 		class GradeTooHighException : public std::exception
 		{
 		public:
@@ -46,8 +48,8 @@ public:
 		public:
 				const char* what() const throw() override;
 		};
-		//-------Overload--------//
-		std::ostream &operator<< (std::ostream &os, const Bureaucrat &b);
 };
+														//* -------Overload--------//
+	std::ostream &operator<< (std::ostream &os, const Bureaucrat &b);
 
 #endif // !BUREAUCRAT_HPP
---
+++ ex00/src/Bureaucrat.cpp
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 21:53:52 by cmakario          #+#    #+#             */
+/*   Updated: 2025/04/05 22:39:52 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Bureaucrat.hpp"
+#include "../include/Colors.hpp"
+
+																													//!-----------OCF-----------------//
+Bureaucrat::Bureaucrat() : name("Default"), grade(150)
+{
+	std::cout << MAGENTA << "Bureaucrat: 🏗 Constructor called." << RESET << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->grade = grade;
+	std::cout << MAGENTA << "Bureaucrat: 🏗 Constructor called." << RESET << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src) : name(src.name), grade(src.grade)
+{
+	std::cout << MAGENTA << "Bureaucrat: 🏗 Copy Constructor called." << RESET << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator= (const Bureaucrat &src)
+{
+	if (this == &src)
+	{
+		std::cout << YELLOW << "⚠️ Self-assignment detected, no changes made!" << RESET << std::endl;
+		return (*this);
+	}
+	this->grade = src.grade; // name is const, so i don't change it
+	std::cout << MAGENTA << "Bureaucrat: © Copy Assignment operator called." << RESET << std::endl;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << RED << "Bureaucrat: 🧨 Destructor called." << RESET << std::endl;
+}
+																													//!===============================//
+																													//!-----------Methods------------//	
+																													//!----Getter-------------------//
+std::string Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (this->grade);
+}
+																													//!----------Inc/decrement-----//
+void Bureaucrat::incrementGrade()
+{
+	if (this->grade <= 1)
+		throw Bureaucrat::GradeTooHighException();
+	--this->grade;
+	std::cout << GREEN << "Bureaucrat: Grade incremented to " << this->grade << RESET << std::endl;
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (this->grade >= 150)
+		throw Bureaucrat::GradeTooLowException();
+	++this->grade;
+	std::cout << GREEN << "Bureaucrat: Grade decremented to " << this->grade << RESET << std::endl;
+}
+																													//! -------Exceptions----------//
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return (RED "Bureaucrat: Grade too high!" RESET);
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return (RED "Bureaucrat: Grade too low!" RESET);
+}
+																													//! -------Overload--------//
+std::ostream &operator<< (std::ostream &os, const Bureaucrat &b)
+{
+	os << MAGENTA << "Bureaucrat: " << b.getName() << ", grade: " << b.getGrade() << RESET;
+	return (os);
+}
